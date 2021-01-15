dfe5f82efc4a71dad389fd1277565464d33f2cb3
Pradhyuman911 <55801621+Pradhyuman911@users.noreply.github.com>
2021-01-15 10:15:57 +0530

    again changed

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       routes: {
         "/" : (_) => HomeScreen(),
-        // "/drawer2": (_) => DrawerApp(),
+        "/drawer2": (_) => DrawerApp(),
       },
     );
   }
